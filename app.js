7a13f38716567c53533106c85f801c004a2bf962
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-09-21 17:17:32 -0700

    troubleshooting why beanies are not appearing

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 /* Imports */
+import { getBeanies } from './fetch-utils.js';
+import { renderBeanies } from './render.utils.js';
 
 /* Get DOM Elements */
+const notificationDisplay = document.getElementById('notification-display');
+const beanieList = document.getElementById('beanie-list');
 
 /* State */
+let error = null;
+let beanies = [];
+
+displayBeanies();
 
 /* Events */
+window.addEventListener('load', async () => {
+    findBeanies();
+
+}); 
+
+async function findBeanies(name) {
+    const response = await getBeanies(name);
+
+    error = response.error;
+    beanies = response.data;
+
+    displayNotifications();
+    if (!error) {
+        displayBeanies();
+    }
+}
 
 /* Display Functions */
+function displayBeanies() {
+    beanieList.innerHTML = '';
 
+    for (const beanie of beanies) {
+        const beanieEl = renderBeanies(beanie);
+        beanieList.append(beanieEl);
+    }
+}
+
+function displayNotifications() {
+    if (error) {
+        notificationDisplay.classList.add('error');
+        notificationDisplay.textContent = error.message;
+    } else {
+        notificationDisplay.classList.remove('error');
+        notificationDisplay.textContent = `Showing ${beanies.length} of ${count} matching Beanie Babies`;
+    }
+}    
 // (don't forget to call any display functions you want to run on page load!)
---
+++ render.utils.js
@@ -0,0 +1,18 @@
+export function renderBeanies(beanies) {
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    // const img = document.createElement('img');
+    // img.src = `https://flagcdn.com/72x54/${country.iso2.toLowerCase()}.png`;
+    // img.alt = country.name;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = beanies.name;
+
+    const p = document.createElement('p');
+    p.textContent = beanies;
+
+    li.append(h2, img, p);
+
+    return li;
+}
